f658af9bda3237ac47f761be3184c1540739c685
Kenert Karu <karukenert@gmail.com>
2023-04-22 15:34:25 +0300

    minor changes

--- src/git/git-scanner.sh
+++ src/git/git-scanner.sh
@@ -21,14 +21,14 @@ specificRepoDir="$repoDirectory/$repoName"
 # delete if already exists
 rm -rf "$specificRepoDir"
 
-# really bad solution, we could just pass the repo url to the command instead
-# so we wouldnt have to download it :) 
 git clone ${repoUrl} ${specificRepoDir}
 cd "$specificRepoDir"
 
 resultName=${companyName}_${provider}_${repoName}.txt
 
-gitleaks detect -v > $resultName
+# Seems to only work locally(by cloning repos)
+gitleaks detect -v --config "/Users/kenertkaru/Documents/personal-projects/Wabadus-Hackathon/src/git/gitleaks.toml" > $resultName
+# gitleaks detect -v "$repoUrl" --config "/Users/kenertkaru/Documents/personal-projects/Wabadus-Hackathon/src/git/gitleaks.toml" > $resultName
 
 # only add to results, if the file has contents
 if test -s "./$resultName"; then
